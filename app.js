fd0f887894e74fd0ebce5c33ea7e0bbbe13af650
mark groves <groves999@gmail.com>
2018-12-22 13:40:33 -0500

    alignment improvememts

--- app.js
+++ app.js
@@ -18,25 +18,25 @@ $(window).on('load', function () {
 
 
 var messages = [{
-    header: "You looked afraid!",
+    header: "You look afraid!",
     message: "I don't know whats making you so scared but maybe a delicious Whiskey drink will hep calm you down!"
 }, {
-    header: "You looked Sad!",
+    header: "You look Sad!",
     message: "It's time to perk up! Try some refreshing Gin based cocktails to help forget about your problems!"
 }, {
-    header: "You looked Neutral!",
+    header: "You look Neutral!",
     message: "I don't know what makes you so aloof, but its creeping me out! Try any of these delicious cocktails to put some emotion in ya"
 }, {
-    header: "You looked disgusted!",
+    header: "You look disgusted!",
     message: "Whatever you looked were looking at thoroughly disgusted you. Hopefully this scotch based cocktail will wash that disgust out!"
 }, {
-    header: "You looked Angry!",
+    header: "You look Angry!",
     message: "You need to calm the heck down! Cool off with a one of these amazing Rum based tropical cocktails!"
 }, {
-    header: "You looked Happy!",
+    header: "You look Happy!",
     message: "Someone looks pretty happy today! Keep that happiness going with one of these Vodka based cocktails!"
 }, {
-    header: "You looked Surprised!",
+    header: "You look Surprised!",
     message: "Whoa! I don't know what surprised you so much but heres some Champagne based cocktails to help wash it down!"
 
 }];
